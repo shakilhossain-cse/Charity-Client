003206cf824258735086de81136ee094555216b6
shakilhossain-cse <shakilhossainp.cse@gmail.com>
2022-02-26 17:54:38 +0600

    setup router with lazy

---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,22 @@
+import React, { Suspense, lazy } from "react";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+
+const Home = lazy(() => import("../pages/Home/Home"));
+const About = lazy(() => import("../pages/About/About"));
+
+const AllRoutes = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default AllRoutes;
