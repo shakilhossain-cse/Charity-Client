ebb00556f57fd863b2fd1985222bba33ba87f0b5
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2022-02-27 14:13:32 +0600

    404 page added

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,10 +1,11 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
+import NotFound from "../pages/NotFound/NotFound";
 
 const Home = lazy(() => import("../pages/Home/Home"));
 const About = lazy(() => import("../pages/About/About"));
 
-const AllRoutes:React.FC = () => {
+const AllRoutes: React.FC = () => {
   return (
     <>
       <BrowserRouter>
@@ -12,6 +13,7 @@ const AllRoutes:React.FC = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
