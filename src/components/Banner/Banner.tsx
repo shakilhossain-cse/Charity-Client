785e5c5bbf62e3e8fba0555e6287a98ed649d993
shakilhossain-cse <shakilhossainp.cse@gmail.com>
2022-03-03 11:04:06 +0600

    banner added

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -11,7 +11,7 @@ import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
 const Banner: React.FC = () => {
   return (
-    <>
+    <div>
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -26,17 +26,68 @@ const Banner: React.FC = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="h-[52rem]"
       >
-  
-        <SwiperSlide className="bg-[url('http://muzaddidul.com/Charity-sympathy/images/slider/3.jpg')] bg-no-repeat bg-cover banner">
+        <SwiperSlide className="bg-[url('http://muzaddidul.com/Charity-sympathy/images/slider/3.jpg')] bg-no-repeat bg-cover banner flex items-center justify-center">
+        <div className="text-white z-30 md:w-1/2 p-4">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+              quidem!
+            </p>
+            <h2 className="md:text-6xl text-4xl font-bold my-4">
+              Help The <span className="text-red-700 ">Poor</span> People
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+              praesentium recusandae voluptatum cumque iste eligendi repellendus
+              error dolorem. Omnis dolore suscipit a iusto saepe magni libero
+              facilis nulla esse alias.
+            </p>
+            <button className="bg-red-600 px-8 py-3 rounded-full mt-4">
+              Join Us Today
+            </button>
+          </div>
         </SwiperSlide>
-        <SwiperSlide className="bg-[url('http://muzaddidul.com/Charity-sympathy/images/slider/2.jpg')] bg-no-repeat bg-cover banner">
-          Slide 2
+        <SwiperSlide className="bg-[url('http://muzaddidul.com/Charity-sympathy/images/slider/2.jpg')] bg-no-repeat bg-cover banner flex items-center justify-center">
+        <div className="text-white z-30 p-4 md:w-1/2">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+              quidem!
+            </p>
+            <h2 className="md:text-6xl text-4xl font-bold my-4">
+              Help The <span className="text-red-700 ">Poor</span> People
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+              praesentium recusandae voluptatum cumque iste eligendi repellendus
+              error dolorem. Omnis dolore suscipit a iusto saepe magni libero
+              facilis nulla esse alias.
+            </p>
+            <button className="bg-red-600 px-8 py-3 rounded-full mt-4">
+              Join Us Today
+            </button>
+          </div>
         </SwiperSlide>
-        <SwiperSlide className="bg-[url('http://muzaddidul.com/Charity-sympathy/images/slider/1.jpg')] bg-no-repeat bg-cover banner">
-          Slide 3
+        <SwiperSlide className="bg-[url('http://muzaddidul.com/Charity-sympathy/images/slider/1.jpg')] bg-no-repeat bg-cover banner flex items-center justify-center">
+        <div className="text-white z-30 p-4  md:w-1/2">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+              quidem!
+            </p>
+            <h2 className="md:text-6xl text-4xl font-bold my-4">
+              Help The <span className="text-red-700 ">Poor</span> People
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+              praesentium recusandae voluptatum cumque iste eligendi repellendus
+              error dolorem. Omnis dolore suscipit a iusto saepe magni libero
+              facilis nulla esse alias.
+            </p>
+            <button className="bg-red-600 px-8 py-3 rounded-full mt-4">
+              Join Us Today
+            </button>
+          </div>
         </SwiperSlide>
       </Swiper>
-    </>
+    </div>
   );
 };
 
