4f72001cd57693d9871b2a2b199f72e0ca581ba7
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2022-03-04 04:31:47 +0600

    contact added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
+import Contact from "../Contact/Contact";
 
 const Home: React.FC = () => {
   return (
@@ -9,6 +11,8 @@ const Home: React.FC = () => {
       <Header />
       <Navbar />
       <Banner />
+      <Contact />
+      <Footer />
     </div>
   );
 };
