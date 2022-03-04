41bc77fdd757dd07133cce2348d70ae5cc95a183
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2022-03-05 03:30:59 +0600

    causes changed to one component

--- src/components/Causes/Causes.tsx
+++ src/components/Causes/Causes.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import CausesCard from '../CausesCard/CausesCard';
 
 const Causes: React.FC = () => {
     return (
@@ -17,8 +16,32 @@ const Causes: React.FC = () => {
                     </div>
 
                     <div className="grid grid-cols-1 gap-8 pt-10 mt-16 border-t border-gray-100 sm:grid-cols-2 lg:grid-cols-3 sm:gap-16" >
-                        {/* map will be here */}
-                        <CausesCard />
+                        {/* Map will be here */}
+                        <div>
+                            <img
+                                src="http://muzaddidul.com/Charity-sympathy/images/causes/2.jpg"
+                                alt=""
+                                className="w-80 h-50 mx-auto shadow-xl"
+                            />
+
+                            <blockquote
+                                className="flex flex-col justify-between p-10 -mt-6 text-left rounded-lg shadow-xl "
+                            >
+                                <p className="text-lg text-red-500 font-semibold">
+                                    $189.99
+                                </p>
+                                <p className="mt-4 text-sm text-gray-500">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                                    voluptatem alias ut provident sapiente repellendus.
+                                </p>
+
+                                <div className="flex space-x-0.5  mt-3 text-white justify-start">
+                                    <button className="bg-red-600 px-8 py-2 rounded-full mt-4">
+                                        Join Us
+                                    </button>
+                                </div>
+                            </blockquote>
+                        </div>
                     </div>
                 </div>
             </section>
