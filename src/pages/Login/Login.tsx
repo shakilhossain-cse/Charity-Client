db6d7d575dd295046e6ef2fb52301b88fd461d31
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2022-03-04 22:20:05 +0600

    register added

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import login from '../../assets/Charity_login.png';
 
 const Login: React.FC = () => {
     return (
         <div>
-
             <div className="flex flex-wrap w-full">
                 <div className="flex flex-col w-full md:w-1/2">
                     <div className="flex justify-center pt-12 md:justify-start md:pl-12 md:-mb-24">
-                        <a href="#" className="p-4 text-xl font-bold text-white bg-black">
+                        <Link to='/' className="p-4 text-xl font-bold text-white bg-black">
                             Charity Home
-                        </a>
+                        </Link>
                     </div>
                     <div className="flex flex-col justify-center px-8 pt-8 my-auto md:justify-start md:pt-0 md:px-24 lg:px-32">
                         <p className="text-3xl text-center">
@@ -47,18 +48,17 @@ const Login: React.FC = () => {
                         <div className="pt-12 pb-12 text-center">
                             <p>
                                 Don&#x27;t have an account?
-                                <a href="#" className="font-semibold underline">
+                                <Link to='/register' className="font-semibold underline">
                                     Register here.
-                                </a>
+                                </Link>
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className="w-1/2 shadow-2xl">
-                    <img className="hidden object-cover w-full h-screen md:block" src="https://www.tailwind-kit.com/images/object/9.jpg" />
+                    <img className="hidden object-cover w-full h-screen md:block" src={login} />
                 </div>
             </div>
-
         </div>
     );
 };
